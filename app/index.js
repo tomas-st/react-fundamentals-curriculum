ee6b5288f4cf970f4c549bc0c24dab293cd0f77b
Tomas Stiller <mail@tomas.de>
2016-09-24 10:58:34 +0200

    fixings for step 1

--- app/index.js
+++ app/index.js
@@ -1,21 +1,13 @@
-/**
- * Created by tom on 24.09.16.
- */
-
 var React = require('react')
 var PropTypes = React.PropTypes
 var ReactDom = require('react-dom')
 
 var HelloWorld = React.createClass({
-  getInitialState: function () {
-    return {
-    }
-  },
-  render: function() {
+  render: function () {
     return (
-        <div>Hello World!!!</div>
-    );
+      <div>Hello</div>
+    )
   }
-});
+})
 
 ReactDom.render(<HelloWorld />, document.getElementById('app'))
